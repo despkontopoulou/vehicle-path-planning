837e4b0015076b030e6dd6e3986aa51f9232d4ca
despkontopoulou <despkontopoulou@gmail.com>
2025-04-16 17:32:17 +0300

    app initialization

---
+++ src/test/java/com/despkontopoulou/vehiclepathplanning/VehiclePathPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despkontopoulou.vehiclepathplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclePathPlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
