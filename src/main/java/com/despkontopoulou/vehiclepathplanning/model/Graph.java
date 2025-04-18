8f4ffc2cc53f6a89ab86bc18d71a56bf3ab2da0c
despkontopoulou <despkontopoulou@gmail.com>
2025-04-18 16:31:20 +0300

    graph class methods

--- src/main/java/com/despkontopoulou/vehiclepathplanning/model/Graph.java
+++ src/main/java/com/despkontopoulou/vehiclepathplanning/model/Graph.java
@@ -4,6 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Graph {
-    private final Map<Long, Map<Long, Double>> graph = new HashMap<>();
-    private final Map<Long, Coordinate> coordinates = new HashMap<>();
+    private final Map<Long, Map<Long, Double>> graph = new HashMap<>(); // graph data in the form of {node:{neighbour:weight}}
+    private final Map<Long, Coordinate> coordinates = new HashMap<>();//the coordinates of each node in the graph
+
+    public void addNode(Long nodeId, Coordinate coordinate) {//add a node to the graph
+        coordinates.put(nodeId, coordinate);//store coordinate
+        graph.putIfAbsent(nodeId, new HashMap<>());//put in graph and create empty list of neighbours
+    }
+
+    public void addEdge(Long fromNodeId, Long toNodeId, Double weight) {//add directed edges
+        if(!graph.containsKey(fromNodeId)) {
+            throw new IllegalArgumentException("fromNodeId "+ fromNodeId+" is not found");
+        }
+        graph.get(fromNodeId).put(toNodeId, weight);//assuming nodes are already added, add node and weight to neighbours
+    }
+
+    public Map<Long, Double> getNeighbours(Long nodeId) {//return neighbours of a node
+        return graph.getOrDefault(nodeId, new HashMap<>());//if a neighbour isnt found, return empty map
+    }
+    public Coordinate getCoordinate(Long nodeId) {//return stored coordinates of a node
+        return coordinates.getOrDefault(nodeId, new Coordinate(0,0));//if node isnt found return 0,0
+    }
 }
